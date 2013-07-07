225e1ef4109b443ad048efff3b36ad2070efd8f8
Keith Tom <keith.tom@gmail.com>
2013-07-07 14:29:33 -0700

    just fix whitespace and indentation

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,105 +1,87 @@
 require_relative './grid'
 
 class Rover
+  attr_reader :x, :y, :orientation
+  attr_accessor :grid
 
-	attr_reader :x, :y, :orientation
-	attr_accessor :grid
+  def initialize(x, y, orientation, grid)
+    @x = x
+    @y = y
+    @orientation = orientation
+    @grid = grid
+  end
 
-	def initialize(x, y, orientation, grid)
-		@x = x
-		@y = y
-		@orientation = orientation
-		@grid = grid
-	end
+  def rotate_rover(orientation, direction)
+    rotations= ["N", "E", "S", "W"]
+    direction == "R" ? turn_right(rotations, orientation) : turn_left(rotations, orientation)
+  end
 
-	def rotate_rover(orientation, direction)
-		rotations= ["N", "E", "S", "W"]
-		direction == "R" ? turn_right(rotations, orientation) : turn_left(rotations, orientation)
-	end
+  def move_to_new_grid(orientation)
+    @grid.grid.reverse[@y][@x] = nil
+    case orientation
+    when "N"
+      increase_y_coordinate
+    when "E"
+      increase_x_coordinate
+    when "S"
+      decrease_y_coordinate
+    when "W"
+      decrease_x_coordinate
+    end
+  end
 
-	def move_to_new_grid(orientation)
-		@grid.grid.reverse[@y][@x] = nil 
-		case orientation
-		when "N" 
-			increase_y_coordinate
-		when "E" 
-			increase_x_coordinate
-		when "S" 
-			decrease_y_coordinate
-		when "W" 
-			decrease_x_coordinate
-		end
-	end
+  def final_position(x, y, orientation)
+    @grid.grid.reverse[y][x] = orientation
+  end
 
-	def final_position(x, y, orientation)
-		@grid.grid.reverse[y][x] = orientation
-	end
+  def coordinates
+    "#{@x} #{@y} #{@orientation}"
+  end
 
-	def coordinates
-		"#{@x} #{@y} #{@orientation}"
-	end
+  private
 
-	private 
+  def turn_left(array, orientation)
+    if first_element?(array, orientation)
+      @orientation = array[-1]
+    else
+      @orientation = array[(array.index(orientation) - 1)]
+    end
+  end
 
-	def turn_left(array, orientation)
-		if first_element?(array, orientation)
-			@orientation = array[-1]
-		else
-			@orientation = array[(array.index(orientation) - 1)] 
-		end
-	end
+  def turn_right(array, orientation)
+    if last_element?(array, orientation)
+      @orientation = array[0]
+    else
+      @orientation = array[(array.index(orientation) + 1)]
+    end
+  end
 
-	def turn_right(array, orientation)
-		if last_element?(array, orientation)
-			@orientation = array[0]
-		else
-			@orientation = array[(array.index(orientation) + 1)] 
-		end		
-	end
+  def decrease_y_coordinate
+    raise StandardError, "invalid move coordinate values must be greater than or equal to zero" if @y == 0
+    @y -= 1 if @grid.available?(@x, (@y-1))
+  end
 
-	def decrease_y_coordinate
-		raise StandardError, "invalid move coordinate values must be greater than or equal to zero" if @y == 0
-		@y -= 1 if @grid.available?(@x, (@y-1))
-	end
+  def decrease_x_coordinate
+    raise StandardError, "invalid move coordinate values must be greater than or equal to zero" if @x == 0
+    @x -= 1 if @grid.available?(@x-1, @y)
+  end
 
-	def decrease_x_coordinate
-		raise StandardError, "invalid move coordinate values must be greater than or equal to zero" if @x == 0
-		@x -= 1 if @grid.available?(@x-1, @y)	
-	end
+  def increase_y_coordinate
+    raise StandardError, "invalid move: coordinate values cannot exceed the grid size" if @y == @grid.height
+    @y+=1 if @grid.available?(@x, (@y+1))
+  end
 
-	def increase_y_coordinate
-		raise StandardError, "invalid move: coordinate values cannot exceed the grid size" if @y == @grid.height
-		@y+=1 if @grid.available?(@x, (@y+1))
-	end
+  def increase_x_coordinate
+    raise StandardError, "invalid move: coordinate values cannot exceed the grid size" if @x == @grid.width
+    @x+=1 if @grid.available?(@x+1, @y)
+  end
 
-	def increase_x_coordinate
-		raise StandardError, "invalid move: coordinate values cannot exceed the grid size" if @x == @grid.width
-		@x+=1 if @grid.available?(@x+1, @y)
-	end
+  def last_element?(array, element)
+    array.index(element) == (array.length - 1)
+  end
 
-	def last_element?(array, element)
-		array.index(element) == (array.length - 1)
-	end
-
-	def first_element?(array, element)
-		array.index(element) == 0
-	end
+  def first_element?(array, element)
+    array.index(element) == 0
+  end
 
 end
-	
-
-
-# grid = Grid.new(5,5)
-# rover = Rover.new(5, 5, "N", grid)
-# rover.move_to_new_grid("S")
-# rover.final_position(5, 4, "S")
-# puts "#{rover.x}, #{rover.y} #{rover.orientation}"
-# p rover.grid.grid
-
-# puts "rover 2"
-# rover2 = Rover.new(5, 5, "E", grid)
-# rover2.move_to_new_grid("S")
-# rover.final_position(5, 4, "S")
-# p rover2.grid.grid
-# puts "#{rover.x}, #{rover.y} #{rover.orientation}"
-
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,82 +1,79 @@
 require 'rspec'
 require_relative '../lib/rover'
 
+describe Rover do
+  before(:each) do
+    @grid = mock(Grid)
+    @grid.stub(:height).and_return(5)
+    @grid.stub(:width).and_return(5)
+    @grid.stub(:grid).and_return(Array.new(@grid.height + 1){Array.new(@grid.width + 1)})
+    @rover = Rover.new(1,1,"N", @grid)
+  end
 
-describe Rover do 
-	before(:each) do
-		@grid = mock(Grid)
-		@grid.stub(:height).and_return(5)
-		@grid.stub(:width).and_return(5)
-		@grid.stub(:grid).and_return(Array.new(@grid.height + 1){Array.new(@grid.width + 1)})
-		@rover = Rover.new(1,1,"N", @grid)
-	end
+  describe "#rotate_rover" do
+    it "rotates the rover left if the direction given is L" do
+      @rover.rotate_rover(@rover.orientation, "L")
+      @rover.orientation.should eq("W")
+    end
 
-	describe "#rotate_rover" do
+    it "rotates the rover right if the direction given is R" do
+      @rover.rotate_rover(@rover.orientation, "R")
+      @rover.orientation.should eq("E")
+    end
+  end
 
-		it "rotates the rover left if the direction given is L" do
-			@rover.rotate_rover(@rover.orientation, "L")
-			@rover.orientation.should eq("W")
-		end
+  context "moves are valid" do
+    describe "#move_to_new_grid" do
+      it "will remove the original position if the same bot is selected twice to move " do
+        x = @rover.x
+        y = @rover.y
+        @rover.grid.grid.reverse[y][x] = "W"
+        @grid.stub(:available?).and_return(true)
+        @rover.move_to_new_grid("N")
+        @rover.grid.grid.reverse[y][x].should be_nil
+      end
 
-		it "rotates the rover right if the direction given is R" do
-			@rover.rotate_rover(@rover.orientation, "R")
-			@rover.orientation.should eq("E")
-		end
-	end
-	context "moves are valid" do
-		describe "#move_to_new_grid" do
-			it "will remove the original position if the same bot is selected twice to move " do
-				x = @rover.x
-				y = @rover.y
-				@rover.grid.grid.reverse[y][x] = "W"
-				@grid.stub(:available?).and_return(true)
-				@rover.move_to_new_grid("N")
-				@rover.grid.grid.reverse[y][x].should be_nil
-			end
+      it "move the rover forward depending on it's orientation" do
+        @grid.stub(:available?).and_return(true)
+        @rover.move_to_new_grid("W")
+        @rover.x.should eq(0)
+      end
+    end
+  end
 
-			it "move the rover forward depending on it's orientation" do
-				@grid.stub(:available?).and_return(true)
-				@rover.move_to_new_grid("W")
-				@rover.x.should eq(0)
-			end
-		end
-	end
+  context "moves are not valid" do
+    describe "#moves_to_new_grid" do
+      it "raises an error if the rover tries to move off the south edge of the grid" do
+        rover = Rover.new(0,0, "N", @grid)
+        expect{@rover.move_to_new_grid("S")}.to raise_error
+      end
 
-	context "moves are not valid" do
-		describe "#moves_to_new_grid" do
-			it "raises an error if the rover tries to move off the south edge of the grid" do
-				rover = Rover.new(0,0, "N", @grid)
-				expect{@rover.move_to_new_grid("S")}.to raise_error
-			end
+      it "raises an error if the rover tries to move off the north edge of the moves_to_new_grid" do
+        rover = Rover.new(5,5, "N", @grid)
+        expect{rover.move_to_new_grid("N")}.to raise_error
+      end
 
-			it "raises an error if the rover tries to move off the north edge of the moves_to_new_grid" do
-				rover = Rover.new(5,5, "N", @grid)
-				expect{rover.move_to_new_grid("N")}.to raise_error
-			end
+      it "raises an error if the rover tries to move off the east edge of the grid" do
+        rover = Rover.new(5,5, "N", @grid)
+        expect{rover.move_to_new_grid("E")}.to raise_error
+      end
+      it "raises an error if the rover tries to move off the west edge of the grid" do
+        rover = Rover.new(0,0, "N", @grid)
+        expect{@rover.move_to_new_grid("W")}.to raise_error
+      end
+    end
+  end
 
-			it "raises an error if the rover tries to move off the east edge of the grid" do
-				rover = Rover.new(5,5, "N", @grid)
-				expect{rover.move_to_new_grid("E")}.to raise_error
-			end
-			it "raises an error if the rover tries to move off the west edge of the grid" do
-				rover = Rover.new(0,0, "N", @grid)
-				expect{@rover.move_to_new_grid("W")}.to raise_error
-			end
-		end
-	end
+  describe "#final_position" do
+    it "should mark the final position of the rover on the board" do
+      @rover.final_position(1, 1, "N")
+      @rover.grid.grid.reverse[1][1].should eq("N")
+    end
+  end
 
-
-	describe "#final_position" do
-		it "should mark the final position of the rover on the board" do
-			@rover.final_position(1, 1, "N")
-			@rover.grid.grid.reverse[1][1].should eq("N")
-		end
-	end
-
-	describe "#coordinates" do 
-		it "should return the coordinates of the rover" do
-			@rover.coordinates.should eq("1 1 N")
-		end
-	end
+  describe "#coordinates" do
+    it "should return the coordinates of the rover" do
+      @rover.coordinates.should eq("1 1 N")
+    end
+  end
 end
-
