e82a79f3bb54346418d12ddbce6ddbf535697f93
Keith Tom <keith.tom@gmail.com>
2013-07-07 14:06:13 -0700

    just cleanup trailing whitespace and indent w 2 spaces

--- spec/navigator_spec.rb
+++ spec/navigator_spec.rb
@@ -1,34 +1,33 @@
 require 'rspec'
 require_relative '../lib/navigator'
 
-describe Navigator do 
-	let(:navigator){Navigator.new}
-	before(:each) do
-		navigator.select_grid_size(5,5)
-		navigator.select_rover(1,2,"N")
-	end
+describe Navigator do
+  let(:navigator){Navigator.new}
+  before(:each) do
+    navigator.select_grid_size(5,5)
+    navigator.select_rover(1,2,"N")
+  end
 
-	describe "#select_grid_size" do
-		it "creates a new grid" do
-			navigator.grid.should be_a(Grid)
-		end
-	end
+  describe "#select_grid_size" do
+    it "creates a new grid" do
+      navigator.grid.should be_a(Grid)
+    end
+  end
 
-	describe "#select_rover" do
-		it "creates a new rover" do			
-			navigator.rover.should be_a(Rover)
-		end
-	end
+  describe "#select_rover" do
+    it "creates a new rover" do
+      navigator.rover.should be_a(Rover)
+    end
+  end
 
-	describe "#direct_rover" do
-		it "takes valid directions and moves the rover properly" do
-			navigator.direct_rover("LMLMLMLMM")
-			navigator.rover.coordinates(navigator.rover.x, navigator.rover.y, navigator.rover.orientation).should eq("1 3 N")
-		end
+  describe "#direct_rover" do
+    it "takes valid directions and moves the rover properly" do
+      navigator.direct_rover("LMLMLMLMM")
+      navigator.rover.coordinates(navigator.rover.x, navigator.rover.y, navigator.rover.orientation).should eq("1 3 N")
+    end
 
-		it "will return an error if the directions are invalid" do
-			expect{navigator.direct_rover("LMLMxMLMM")}.to raise_error
-		end
-	end
-
+    it "will return an error if the directions are invalid" do
+      expect{navigator.direct_rover("LMLMxMLMM")}.to raise_error
+    end
+  end
 end
