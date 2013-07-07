be77712f03538dddd5bcdc623f4156790db3c246
Keith Tom <keith.tom@gmail.com>
2013-07-07 15:07:42 -0700

    add comments about rover

--- lib/rover.rb
+++ lib/rover.rb
@@ -12,12 +12,19 @@ class Rover
     @grid = grid
   end
 
+  # i might just rename this to #rotate since you are already calling it on a rover object
+  # rover.rotate_rover vs rover.rotate
+  # you want your methods to sound like commands.  OOP is a perspective of telling objects to do things.
   def rotate_rover(direction)
+    # these new method names should sound more like commands
+    # rover.turn_right is telling the rover to do it.  and it should figure it out on its own; ie. you don't need to give it parameters, it should figure it out
     direction == "R" ? turn_right : turn_left
   end
 
+  # this should be more like.  rover.move
+  # no argument needed.
   def move_to_new_grid(orientation)
-    @grid.grid.reverse[@y][@x] = nil
+    @grid.grid.reverse[@y][@x] = nil  # why are we doing this here?
     case orientation
     when "N"
       increase_y_coordinate
@@ -30,6 +37,9 @@ class Rover
     end
   end
 
+  # it feels like a rover should update its final position on the grid after any move or rotation method.
+  # the way it is now, the navigator is responsble for making sure this method is called.
+  # make a branch and see what the code looks like when the rover calls this method, i think it'll seem cleaner
   def final_position(x, y, orientation)
     @grid.grid.reverse[y][x] = orientation
   end
@@ -40,6 +50,7 @@ class Rover
 
   private
 
+  # you can make this simpler with modulus operator.  try #turn_right first.
   def turn_left
     if first_element?(ROTATIONS, orientation)
       @orientation = ROTATIONS[-1]
@@ -48,6 +59,11 @@ class Rover
     end
   end
 
+  # you can make this simpler if you use a modulus operator
+  # the way I read this code, you are saying:
+  # to turn right, I have to take the next element in the ROTATIONS array
+  # unless I am at the last one, then take the first.
+  # this is the same as saying (ROTATIONS.index(orientation) + 1) % ROTATIONS.length
   def turn_right
     if last_element?(ROTATIONS, orientation)
       @orientation = ROTATIONS[0]
@@ -56,6 +72,7 @@ class Rover
     end
   end
 
+  # nice methods here.  overall good.  if you change #available? to return false, then you need to handle that here with a new exception
   def decrease_y_coordinate
     raise StandardError, "invalid move coordinate values must be greater than or equal to zero" if @y == 0
     @y -= 1 if @grid.available?(@x, (@y-1))
@@ -76,6 +93,7 @@ class Rover
     @x+=1 if @grid.available?(@x+1, @y)
   end
 
+  # you can get rid of these methods if you make the modulus changse
   def last_element?(array, element)
     array.index(element) == (array.length - 1)
   end
