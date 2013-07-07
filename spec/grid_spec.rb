c130d1c1ebd7694a0661d7c13b4a13daa78cd686
Keith Tom <keith.tom@gmail.com>
2013-07-07 14:21:08 -0700

    just fix whitespace and indent by 2 spaces

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,18 +1,17 @@
 require 'rspec'
 require_relative '../lib/grid'
 
-describe Grid do 
-	describe "#available?" do
-		let(:grid){Grid.new(6,6)}
-		
-		it "returns true if the grid is empty" do
-			grid.available?(3,3).should eq(true)
-		end 
+describe Grid do
+  describe "#available?" do
+    let(:grid){Grid.new(6,6)}
 
+    it "returns true if the grid is empty" do
+      grid.available?(3,3).should eq(true)
+    end
 
-		it "raises an error if the grid is already taken" do
-			grid.grid[3][3] = "N"
-			expect{grid.available?(3,3)}.to raise_error
-		end
-	end
+    it "raises an error if the grid is already taken" do
+      grid.grid[3][3] = "N"
+      expect{grid.available?(3,3)}.to raise_error
+    end
+  end
 end
